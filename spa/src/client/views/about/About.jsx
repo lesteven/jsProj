08c8768c58c46de748b6d4ada7a4fca419ae6b2d
lesteven <stevenle2011@gmail.com>
2019-08-08 13:32:01 -0700

    feat: add views

---
+++ spa/src/client/views/about/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import css from './about.css';
+
+const About = () => {
+  return (
+    <div className='about'>
+      <p> Hello About! </p>
+    </div>
+  )
+}
+
+export default About;
---
+++ spa/src/client/views/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import css from './home.css';
+
+
+const Home = () => {
+  return (
+    <div className='home'>
+      <p> Hello Home! </p>
+    </div>
+  )
+}
+
+export default Home;
