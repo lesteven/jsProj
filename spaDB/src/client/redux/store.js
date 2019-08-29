0d163229d4cc40181389e3139394d83fb1cec8f9
lesteven <stevenle2011@gmail.com>
2019-08-28 23:02:17 -0700

    fix: redux bug

--- spaDB/src/client/redux/store.js
+++ spaDB/src/client/redux/store.js
@@ -11,7 +11,9 @@ const reducers = combineReducers({
 const configureStore = () => {
   return createStore(
     reducers,
-    compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__())
+    compose(applyMiddleware(thunk), 
+      window.__REDUX_DEVTOOLS_EXTENSION? 
+        window.__REDUX_DEVTOOLS_EXTENSION__(): f => f)
   )
 }
 
