60c53b0abfe0c84b4c0e497432d09760b00460da
lesteven <stevenle2011@gmail.com>
2020-07-28 16:41:28 -0700

    spa: update

--- spa/src/client/routes.js
+++ spa/src/client/routes.js
@@ -1,11 +1,4 @@
 import Home from './views/home/Home';
-import About from './views/about/About';
-import Dynam from './views/dynam/Dynam';
-import Hooks from './views/hooks/Hooks';
-import LifeCy from './views/lifecy/LifeCy';
-import Red from './views/red/Red';
-import Blue from './views/blue/Blue';
-import FormEx from './views/formex/FormEx';
 
 const routes = [
   {
@@ -14,55 +7,6 @@ const routes = [
     component: Home,
     exact: true,
     title: 'Home',
-  },
-  {
-    prefix: '',
-    path: '/about',
-    component: About,
-    exact: true,
-    title: 'About',
-  },
-  {
-    prefix: '',
-    path: '/dynam',
-    component: Dynam,
-    exact: true,
-    title: 'Dynam',
-  },
-  {
-    prefix: '',
-    path: '/hooks',
-    component: Hooks,
-    exact: true,
-    title: 'Hooks',
-  },
-  {
-    prefix: '',
-    path: '/lifecy',
-    component: LifeCy,
-    exact: true,
-    title: 'LifeCy',
-  },
-  {
-    prefix: '',
-    path: '/red',
-    component: Red,
-    exact: true,
-    title: 'Red',
-  },
-  {
-    prefix: '',
-    path: '/blue',
-    component: Blue,
-    exact: true,
-    title: 'Blue',
-  },
-  {
-    prefix: '',
-    path: '/formex',
-    component: FormEx,
-    exact: true,
-    title: 'FormEx',
   },
 ]
 
